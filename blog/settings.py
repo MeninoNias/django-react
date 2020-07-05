bd84ede93830ef181a6b8d13a145ac0164d0612e
Ananias <ananias.nobrega@gmail.com>
2020-07-05 18:16:28 -0300

    created the app core and implemensts models

--- blog/settings.py
+++ blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
---
+++ core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class List(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Lista'
+        verbose_name_plural = 'Listas'
+
+
+class Item(models.Model):
+    list = models.ForeignKey(List, on_delete=models.PROTECT)
+    name = models.CharField(max_length=50)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Item'
+        verbose_name_plural = 'Itens'
