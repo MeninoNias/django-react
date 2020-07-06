e6dffe046f7ceb7b1133fce607d05ce227824ce7
Ananias <ananias.nobrega@gmail.com>
2020-07-06 00:20:54 -0300

    implements api

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,15 @@
-from django.urls import include, path
+from django.urls import include
 from rest_framework import routers
 from .views import GroupViewSet, UserViewSet
+from core.views import ListViewSet, ItemViewSet
 from django.contrib import admin
 from django.urls import path
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
-
+router.register(r'lists', ListViewSet)
+router.register(r'itens', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, authentication
+from .serializers import ListSerializer, ItemSerializer
+from core.models import List, Item
 
-# Create your views here.
+
+class ListViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
