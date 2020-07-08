84633cd255e40ea1c1a9f1d2e47ac0da00aa27ef
Ananias <ananias.nobrega@gmail.com>
2020-07-07 22:57:58 -0300

    token for user

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ from django.urls import path
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
-router.register(r'lists', ListViewSet)
+router.register(r'lists', ListViewSet, basename='list')
 router.register(r'itens', ItemViewSet)
 
 urlpatterns = [
--- core/views.py
+++ core/views.py
@@ -7,11 +7,14 @@ class ListViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = List.objects.all()
     serializer_class = ListSerializer
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
 
+    def get_queryset(self):
+        user = self.request.user
+        return List.objects.filter(owner=user)
+    
 
 class ItemViewSet(viewsets.ModelViewSet):
     """
